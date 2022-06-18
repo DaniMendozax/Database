28623e15a8fb0fba9b967962835c7fc24e7253cb
DaniMendozax <danimendozajejen@gmail.com>
2022-06-18 13:25:59 -0500

    Changing txt file to csv file and finishing update and delete

--- Data.py
+++ Data.py
@@ -3,45 +3,62 @@ def run():
     What action do you wish to perform? 
 
     1. Add user 
-    2. See all
-    3. Update user
+    2. Show all users
+    3. Update an user
     4. Remove all users
+    5. Exit 
     
      Select an option: '''
-    flag_Option = True
-
-    while flag_Option:
+    while True:
         option = input(menu)
-
         if option == '1':
             print('New registration')
-            file = open("Dates.txt", "a")
+           
+            file = open("dates.csv", "a")
             ide = input('Enter id: ')
             name = input('Enter full name: ')
             number = input('Enter phone number: ')
-            adress = input('Enter adress: ')
+            address = input('Enter adress: ')
 
             print('User added')
 
-            file.write(f'Id: {ide}, Name: {name}, Number: {number}, Adress: {adress} \n')
+            file.write(f'Id: {ide}, Name: {name}, Number: {number} , Adress: {address} \n')
             file.close()
-
-            flag_Option = False
-            
+            continue
+             
 
         elif option == '2':
-            print('Saved records')
-            file = open("Dates.txt")
-
+            print('Saved records \n')
+            file = open("dates.csv")
             print(file.read())
 
             file.close()
+            continue
+ 
+        elif option == '3':
+            numero = input("Enter the user id: ")
+            if numero == numero:
+                ide = input('Enter new id: ')
+                name = input('Enter new full name: ')
+                number = input('Enter new phone number: ')
+                address = input('Enter new adress: ')
+                print('Updated user')
+                continue
 
-            flag_Option = False
-
+        elif option == '4':
+            delete = input("Enter the user id: ")
+            if delete == delete:
+                op = '''are you sure to delete this user?
+                Enter 1 for deleted user or enter 2 for do not delete the user: '''
+                opt = input(op)
+                if opt == "1":
+                    print("User deleted")
+                elif opt == "2":
+                    print("Goodbye")
+        elif option == "5":
+            break
         else:
-            print('Enter a valid value')
-
-        
+            print("Enter a valid value")
 if __name__ == '__main__':
     run()
+
